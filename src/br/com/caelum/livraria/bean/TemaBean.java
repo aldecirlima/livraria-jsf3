5e43212000b6d8d979135a5c27a8a44fd2e9ba80
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-04-29 19:58:52 -0300

    Aula 7 finalizada

---
+++ src/br/com/caelum/livraria/bean/TemaBean.java
@@ -0,0 +1,34 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class TemaBean {
+
+	private String tema = "cupertino";
+
+	private String[] temas = new String[] { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer", "bluesky",
+			"bootstrap", "casablanca", "cupertino", "cruze", "dark-hive", "delta", "dot-luv", "eggplant",
+			"excite-bike", "flick", "glass-x", "home", "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+			"overcast", "pepper-grinder", "redmond", "rocket", "sam", "smoothness", "south-street", "start",
+			"sunny", "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader" };
+	
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+	public String[] getTemas() {
+		return this.temas;
+	}
+
+	public void setTemas(String[] temas) {
+		this.temas = temas;
+	}
+
+}
